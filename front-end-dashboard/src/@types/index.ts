714252912c9f91ccfe609b5eed4f11c6a9b02d74
Mikhael Zimmermann <mikhael928@gmail.com>
2024-02-17 18:32:18 -0300

    Types - Types of the user and products

---
+++ front-end-dashboard/src/@types/index.ts
@@ -0,0 +1,12 @@
+export type Product = {
+  id: number;
+  name: string;
+  description: string;
+  price: number;
+};
+
+export type User = {
+  id: number;
+  firstName: string;
+  lastName: string;
+};
